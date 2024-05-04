946eb3dedb7022ab3c5c0226cec6bd00d18a7a71
KiraKostina <kostina.kira@gmail.com>
2024-05-04 15:37:01 +0300

    step 6 localstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react';
-import './App.module.css';
+import React, { useState, useEffect } from 'react';
+import css from './App.module.css';
 import Description from '../components/Description/Description';
 import Options from '../components/Options/Options';
 import Feedback from '../components/Feedback/Feedback';
 import Notification from '../components/Notification/Notification';
 
 export default function App() {
-  const [feedbacks, setfeedbacks] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [feedbacks, setFeedbacks] = useState(() => {
+    const savedFeedbacks = localStorage.getItem("my-feedbacks");
+    return savedFeedbacks !== null ?
+      JSON.parse(savedFeedbacks)
+   : {
+          good: 0,
+          neutral: 0,
+          bad: 0
+        };
   });
 
   const { good, neutral, bad } = feedbacks;
@@ -17,26 +22,38 @@ export default function App() {
   const positiveFeedback = Math.round((good / totalFeedback) * 100);
 
   const updateFeedback = feedbackType => {
-    setfeedbacks({
+    setFeedbacks({
       ...feedbacks,
       [feedbackType]: feedbacks[feedbackType] + 1,
     });
   };
 
   const handleReset = () => {
-    setfeedbacks({
+    setFeedbacks({
       good: 0,
       neutral: 0,
       bad: 0,
     });
   };
 
+  useEffect(() => {
+    localStorage.setItem("my-feedbacks", JSON.stringify(feedbacks));
+  }, [feedbacks]);
+
   return (
     <>
       <Description />
-      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} handleReset={handleReset} />
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        handleReset={handleReset}
+      />
       {totalFeedback > 0 ? (
-        <Feedback feedbacks={feedbacks} totalFeedback={totalFeedback} positiveFeedback={ positiveFeedback } />
+        <Feedback
+          feedbacks={feedbacks}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
       ) : (
         <Notification totalFeedback={totalFeedback} />
       )}
